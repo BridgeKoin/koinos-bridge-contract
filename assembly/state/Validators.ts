4b31841b74b604a296aab85c05e1d8776c90c9c4
eder lobo <ederandreslobo1998@gmail.com>
2024-06-02 18:05:38 -0500

    updating relayer process and adding methods to claim fees

--- assembly/state/Validators.ts
+++ assembly/state/Validators.ts
@@ -1,13 +1,12 @@
 import { Storage } from '@koinos/sdk-as';
 import { bridge } from '../proto/bridge';
-
-const VALIDATORS_SPACE_ID = 100;
+import { Spaces } from "./Spaces"
 
 export class Validators extends Storage.Map<Uint8Array, bridge.empty_object> {
   constructor(contractId: Uint8Array) {
     super(
       contractId,
-      VALIDATORS_SPACE_ID,
+      Spaces.VALIDATORS_SPACE_ID,
       bridge.empty_object.decode,
       bridge.empty_object.encode,
       () => new bridge.empty_object()
--- assembly/state/WrappedTokens.ts
+++ assembly/state/WrappedTokens.ts
@@ -1,13 +1,12 @@
 import { Storage } from '@koinos/sdk-as';
 import { bridge } from '../proto/bridge';
-
-const WRAPPED_TOKENS_SPACE_ID = 102;
+import { Spaces } from "./Spaces"
 
 export class WrappedTokens extends Storage.Map<Uint8Array, bridge.empty_object> {
   constructor(contractId: Uint8Array) {
     super(
       contractId, 
-      WRAPPED_TOKENS_SPACE_ID, 
+      Spaces.WRAPPED_TOKENS_SPACE_ID, 
       bridge.empty_object.decode, 
       bridge.empty_object.encode,
       () => new bridge.empty_object()
